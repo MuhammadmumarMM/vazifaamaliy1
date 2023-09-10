477737bc9848e13e6b28ca9cdcc702e97211d021
MuhammadumarMM <botirovmuhammadumar33.gmail.com>
2023-09-10 18:30:43 +0500

    Add project files.

---
+++ vazifaamaliy1/Program.cs
@@ -0,0 +1,71 @@
+﻿//// Boolean 36  Ruxning yuriwi
+//Console.Write("X1 va Y1 ni kirgizing!  ");
+//var desc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//Console.Write("Qayerga yursin!! X2 va Y2 kiriting!! ");
+//var desc2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//if (desc[0] == desc2[0] || desc[1] == desc2[1])
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
+
+//// Boolean 37     Shohning yturiwi
+//Console.Write("X1 va Y1 ni kirgizing!  ");
+//var desc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//Console.Write("Qayerga yursin!! X2 va Y2 kiriting!! ");
+//var desc2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//if (Math.Abs(desc[0] - 1) == desc2[0] || Math.Abs(desc[1] - 1) == desc2[1] || Math.Abs(desc2[0] - 1) == desc[0] || Math.Abs(desc2[1] - 1) == desc[1])
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
+
+//// Boolean 38     Filning yuriwi
+//Console.Write("X1 va Y1 ni kirgizing!  ");
+//var desc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//Console.Write("Qayerga yursin!! X2 va Y2 kiriting!! ");
+//var desc2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//if (Math.Abs(desc[0] - desc2[0]) == Math.Abs(desc[1] - desc2[1]))
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
+
+
+//// Boolean 39     Farzinning yuriwi
+//Console.Write("X1 va Y1 ni kirgizing!  ");
+//var desc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//Console.Write("Qayerga yursin!! X2 va Y2 kiriting!! ");
+//var desc2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//if (Math.Abs(desc[0] - desc2[0]) == Math.Abs(desc[1] - desc2[1])  || desc[0] == desc2[0] || desc[1] == desc2[1])
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
+
+
+//// Boolean 38  OTning yuriwi
+//Console.Write("X1 va Y1 ni kirgizing!  ");
+//var desc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//Console.Write("Qayerga yursin!! X2 va Y2 kiriting!! ");
+//var desc2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//if (Math.Abs(desc[0] - desc2[0]) == 1 && Math.Abs(desc[1] - desc2[1]) == 2  || Math.Abs(desc[0] - desc2[0]) == 2 && Math.Abs(desc[1] - desc2[1]) == 1)
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
